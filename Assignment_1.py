aac41f28a534967e96b8113e58d5e0a21ebc3d98
Prasanjeet12 <prasanjeetkumar62@gmail.com>
2025-12-14 16:04:35 +0530

    Implement basic arithmetic operations and greeting
    Added a greeting program that combines first and last names.

--- Assignment_1.py
+++ Assignment_1.py
@@ -5,4 +5,12 @@ b = int(input("Enter the Second number: "))
 print("Addition:",(a+b))
 print("Subtraction:",(a-b))
 print("Multiplication:",(a*b))
-print("Divison:",(a/b)) 
+
+print("Divison:",(a/b))
+
+#Task 2 
+#greeting program 
+First_name = input("enter your first Name:")
+Last_name = input("enter your Last Name:")
+Full_name = First_name + " " + Last_name
+print("Hello,"+ " " + Full_name + "!" + " " + "Welcome to the Python program." )
